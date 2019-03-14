fab65358aeff7701ac6785a4c13b54057259a771
Josh Hall <joshhall90@yahoo.com>
2019-03-14 15:33:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import { Switch, Route } from 'react-router-dom';
+import Schedule from './views/schedule';
+import Events from './views/events';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' render={() => <Schedule />} />
+          <Route exact path='/events' render={() => <Events />} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
